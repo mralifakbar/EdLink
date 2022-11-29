05c31ead9d0fb8d10a4e18a88385cdb0627829d5
mralifakbar <alif.akbarkartadinata2049@students.unila.ac.id>
2022-11-30 02:43:30 +0700

    add home screen

--- app/src/main/java/id/mralifakbar/edlink/MainActivity.kt
+++ app/src/main/java/id/mralifakbar/edlink/MainActivity.kt
@@ -1,11 +1,58 @@
 package id.mralifakbar.edlink
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AlertDialog
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import id.mralifakbar.edlink.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var doubleBackToExitOnce:Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val buttomnNavView: BottomNavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_explore, R.id.navigation_class, R.id.navigation_chat, R.id.navigation_account
+            )
+        )
+        buttomnNavView.setupWithNavController(navController)
+    }
+
+
+    override fun onBackPressed() {
+        if(doubleBackToExitOnce){
+            val builder = MaterialAlertDialogBuilder(this)
+            builder.setTitle("Keluar Aplikasi")
+            builder.setMessage("Apakah yakin ingin keluar?")
+                .setCancelable(false)
+                .setPositiveButton("Yakin", DialogInterface.OnClickListener { dialogInterface, i ->
+                    finish()
+                })
+                .setNegativeButton("Tidak", DialogInterface.OnClickListener { dialogInterface, i ->
+                    dialogInterface.cancel()
+                })
+
+            val alertDialog: AlertDialog = builder.create()
+            alertDialog.show()
+        }
+
+        this.doubleBackToExitOnce = true
+
+        Handler().postDelayed({
+            kotlin.run { doubleBackToExitOnce = false }
+        }, 2000)
     }
 }
