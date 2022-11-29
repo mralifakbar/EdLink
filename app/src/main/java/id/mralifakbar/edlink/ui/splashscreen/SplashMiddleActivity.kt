1884b113a7249f730a49da5184d64a1e57cc1df7
mralifakbar <alif.akbarkartadinata2049@students.unila.ac.id>
2022-11-29 21:40:59 +0700

    add middle splash screen

--- app/src/main/java/id/mralifakbar/edlink/ui/splashscreen/SplashMiddleActivity.kt
+++ app/src/main/java/id/mralifakbar/edlink/ui/splashscreen/SplashMiddleActivity.kt
@@ -2,11 +2,13 @@ package id.mralifakbar.edlink.ui.splashscreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
 import id.mralifakbar.edlink.R
 
 class SplashMiddleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_middle)
+
     }
 }
